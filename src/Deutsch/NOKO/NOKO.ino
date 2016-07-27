aa5e1332bd4eeef1fef5d84e037dcad4b9a078ce
NikolaiRadke <kontakt@nikolairadke.de>
2016-07-27 17:29:45 +0200

    Tested NOKO.ino with IDE 1.6.10 - works!

--- src/Deutsch/NOKO/NOKO.ino
+++ src/Deutsch/NOKO/NOKO.ino
@@ -6,7 +6,7 @@
  * The main loop controls the timing events and gets interrupted by the taste()-funtion.
  * Otherwise NOKO falls asleep with powerdowndelay() for 120ms. This saves a lot of power.
  * 
- * Flash-Usage: 28.384 (1.6.9 | AVR-Boards 1.6.9 | Linux X86_64) 
+ * Flash-Usage: 28.380 (1.6.10 | AVR-Boards 1.6.9 | Linux X86_64) 
  * 
  * Compiler options: -flto -funsafe-math-optimizations -mcall-prologues -maccumulate-args
                      -ffunction-sections -fdata-sections -fmerge-constants
