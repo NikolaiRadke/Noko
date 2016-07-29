29224aa68c1d315b31e625cfe41bfedfec125661
NikolaiRadke <kontakt@nikolairadke.de>
2016-07-29 16:41:14 +0200

    Added 1.6.10 support

--- src/Deutsch/NOKO/NOKO.ino
+++ src/Deutsch/NOKO/NOKO.ino
@@ -5,7 +5,7 @@
  * The main loop controls the timing events and gets interrupted by the taste()-funtion.
  * Otherwise NOKO falls asleep with powerdowndelay() for 120ms. This saves a lot of power.
  * 
- * Flash-Usage: 29.308 (1.6.10 | AVR Core 1.6.12 | Linux x86_64, Windows 10 | Compiler Options)
+ * Flash-Usage: 28.308 (1.6.10 | AVR Core 1.6.12 | Linux x86_64, Windows 10 | Compiler Options)
  * 
  * Optional:
  * Compiler Options:   -funsafe-math-optimizations -mcall-prologues -maccumulate-args
