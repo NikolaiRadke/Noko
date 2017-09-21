bc99d8ae749f94e19202565bff3e8adb018eb9d2
NikolaiRadke <kontakt@nikolairadke.de>
2017-09-21 13:05:33 +0200

    Small update: Changed EEPROM.write() to EEPROM.update() to save EEPROM lifetime

--- src/Deutsch/NOKO/NOKO.ino
+++ src/Deutsch/NOKO/NOKO.ino
@@ -1,11 +1,11 @@
- /* NOKO V1.0 24.08.2017 - Nikolai Radke
+ /* NOKO V1.0 21.09.2017 - Nikolai Radke
  *
  * Sketch for NOKO-Monster - Deutsch
  * NOTE: Does NOT run without the Si4703 Radio Module! Uncommend line 88 if it's not present.
  * The main loop controls the timing events and gets interrupted by the read_button()-funtion.
  * Otherwise NOKO falls asleep with powerdown_delay() for 120ms. This saves a lot of power.
  * 
- * Flash-Usage: 27.130 (1.8.2 | AVR Core 1.6.18 | Linux x86_64, Windows 10 |Compiler options)
+ * Flash-Usage: 27.160 (1.8.2 | AVR Core 1.6.18 | Linux x86_64, Windows 10 |Compiler options)
  * 
  * Optional:
  * Compiler Options:   -funsafe-math-optimizations -mcall-prologues -maccumulate-args
@@ -80,7 +80,7 @@
 */
 
 // Softwareversion
-#define Firmware "-240817"
+#define Firmware "-210917"
 #define Version 10  // 1.0
 #define Build_by "by Nikolai Radke" // Your Name. Max. 20 chars, appears in "Mein NOKO" menu
 
@@ -2356,7 +2356,7 @@ uint8_t read_EEPROM(uint8_t address) // Read internal EEPROM with offset
 
 void write_EEPROM(uint8_t address, uint8_t data) // Write internal EEPROM with offset
 {
-  EEPROM.write(address+offset,data);
+  EEPROM.update(address+offset,data);
 }
 
 
